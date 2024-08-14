1d2b40547ad120147e76796b1791b889ca75f8a0
yanirc1981 <cerzurc@hotmail.com>
2024-08-14 19:02:58 -0300

    register ok

--- laura-back/src/controllers/Users/createUser.js
+++ laura-back/src/controllers/Users/createUser.js
@@ -1,20 +1,22 @@
-const User  = require("../../data");
-const response = require("../../utils");
+// controllers/Users/createUser.js
+const { User } = require('../../data'); // Asegúrate de que esta ruta es correcta
+const response = require("../../utils/response");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
-    try {
-      const user = req.body;
+  try {
+    const user = req.body;
 
-    if (!user.document||
-        !user.documentType||
-        !user.name||
-        !user.lastName||
-        !user.sex||
-        !user.email||
-        !user.password||
-        !user.birthDate) {
+    if (!user.document ||
+        !user.documentType ||
+        !user.name ||
+        !user.lastName ||
+        !user.sex ||
+        !user.email ||
+        !user.password ||
+        !user.birthDate ||
+        !user.role) {
       return response(res, 400, "Todos los campos son obligatorios");
     }
 
@@ -25,7 +27,7 @@ module.exports = async (req, res) => {
     }
 
     // Verificar si el número de documento ya está registrado
-    const existingDocument = await User.findOne({ where: { ndocument: user.document } });
+    const existingDocument = await User.findOne({ where: { document: user.document } });
     if (existingDocument) {
       return response(res, 400, "El número de documento ya está registrado.");
     }
@@ -35,15 +37,22 @@ module.exports = async (req, res) => {
 
     // Crear usuario en la base de datos
     const newUser = await User.create({
-       document :user.document,
-       documentType :user.documentType,
-       name :user.name,
-       lastName :user.lastName,
-       sex :user.sex,
-        email:user.email,
-        password:user.password,
-        birthDate:user.birthDate
+      document: user.document,
+      documentType: user.documentType,
+      name: user.name,
+      lastName: user.lastName,
+      sex: user.sex,
+      email: user.email,
+      password: hash, // Usa el hash en lugar de la contraseña en texto claro
+      birthDate: user.birthDate,
+      phone: user.phone,
+      role: user.role
     });
+
+    // Verifica que la clave secreta esté definida
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error("JWT_SECRET_KEY no está definida en las variables de entorno.");
+    }
 
     // Crear token
     const token = jwt.sign({ document: newUser.document, role: newUser.role }, process.env.JWT_SECRET_KEY, {
