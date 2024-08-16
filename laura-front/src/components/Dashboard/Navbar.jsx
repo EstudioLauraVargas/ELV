8eed527a22af5ce6fdd63ef97ac478cf7eeddbb0
yanirc1981 <cerzurc@hotmail.com>
2024-08-16 12:45:50 -0300

    navbar y pnel

---
+++ laura-front/src/components/Dashboard/Navbar.jsx
@@ -0,0 +1,39 @@
+// Navbar.js
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../Redux/Actions/actions'; 
+import { FaSignOutAlt } from 'react-icons/fa';
+import logolaura from "../../lauraassets/logolaura.png"
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+ 
+  const { userInfo } = useSelector((state) => state.userLogin);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <nav className="bg-gray-800 p-4 text-white flex justify-between items-center fixed top-0  left-0 w-full z-50">
+      <div className="flex items-center">
+        <img src={logolaura} alt="Logo de la empresa" className="h-10" />
+      </div>
+      <div className="flex items-center space-x-4">
+        {userInfo ? (
+          <>
+            <span>{userInfo.name}</span>
+            <button onClick={handleLogout} className="flex items-center">
+              <FaSignOutAlt className="mr-2" />
+              Logout
+            </button>
+          </>
+        ) : (
+          <span>No estás logueado</span>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
