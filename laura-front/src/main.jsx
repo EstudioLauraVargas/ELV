3fe1c037b276b89e8c2a0ff0f84b10d2984f44f3
mlobeto <mercedeslobeto@gmail.com>
2024-08-20 10:53:21 -0300

    main

--- laura-front/src/main.jsx
+++ laura-front/src/main.jsx
@@ -1,23 +1,24 @@
-import ReactDOM from 'react-dom/client';
+/* eslint-disable react/no-deprecated */
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import axios from 'axios';
-import {ToastContainer} from 'react-toastify'
+//import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css'
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store/store';
 
 
-axios.defaults.baseURL = "http://localhost:3001/";
+
 //axios.defaults.baseURL = " https://elv.onrender.com";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
+ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  <ToastContainer/>
-  </Provider>
+    
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    
+  </Provider>,
+  document.getElementById('root')
 );
