3c305345178d283938cbc998a8fb35e4b0e12dd4
mlobeto <mercedeslobeto@gmail.com>
2024-09-20 20:02:27 -0300

    rutas para prod

--- laura-back/src/app.js
+++ laura-back/src/app.js
@@ -4,49 +4,49 @@ const morgan = require("morgan");
 const routes = require("./routes");
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const { passport, initialize } = require("./passport");
+const { passport } = require("./passport");
 const { JWT_SECRET_KEY } = require("./config/envs");
 const path = require('path');
 
-
+const app = express();
 
 // Configuración de CORS
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use(
-  session({
-    secret: `${JWT_SECRET_KEY}`,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+app.use(session({
+  secret: `${JWT_SECRET_KEY}`,
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
-app.use('/', routes);
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 app.use(passport.initialize());
 
-app.use('*', (req, res, next) => {
+// Rutas de API
+app.use('/', routes);
+
+// Servir la aplicación React
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  // Manejo de rutas para React Router
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
+// Manejo de errores
+app.use((req, res, next) => {
   res.status(404).send({
     error: true,
     message: 'Not found',
   });
 });
 
-app.use((err, req, res, netx) => {
+app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).send({
     error: true,
     message: err.message,
@@ -54,4 +54,5 @@ app.use((err, req, res, netx) => {
 });
 
 module.exports = app;
+
 
