9226ffcf9e2fd65cf9e4034089ca83ee23d1fc9b
mlobeto <mercedeslobeto@gmail.com>
2024-09-20 17:40:53 -0300

    main

--- laura-front/src/main.jsx
+++ laura-front/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-//import axios from 'axios';
+import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css'
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store/store';
 
 
 
-//axios.defaults.baseURL = " https://elv.onrender.com";
+axios.defaults.baseURL = "https://elv.onrender.com";
 
 ReactDOM.render(
   <Provider store={store}>
