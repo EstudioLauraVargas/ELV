1764ce22be9ce0325ff504087010af1d333a2b1b
mlobeto <mercedeslobeto@gmail.com>
2024-09-20 23:16:32 -0300

    force

--- laura-back/index.js
+++ laura-back/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/data/index")
 const app = require('./src/app.js');
 const PORT = process.env.PORT || 3000;
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Listening on port: ${PORT} 🚀`);
   });
