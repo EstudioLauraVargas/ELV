bcad2086f4e26c6bc4a00670f52492a1fe617060
mlobeto <mercedeslobeto@gmail.com>
2024-09-24 07:06:26 -0300

    rr

--- laura-back/src/data/index.js
+++ laura-back/src/data/index.js
@@ -52,7 +52,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = require("
   const { User, Course, Subscription, Video, Payment, OrderCompra, CourseVideos } = sequelize.models;
 
 
-/////////////////////////////////////////RELACIONES//////////////////////////////////////
+////////////////////////RELACIONES////////////////////////
 
 
 
