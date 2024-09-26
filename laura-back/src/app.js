9cdd68386823f7bc3393555bc0238d5031369a68
mlobeto <mercedeslobeto@gmail.com>
2024-09-26 10:27:19 -0300

    app

--- laura-back/src/app.js
+++ laura-back/src/app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   next();
 });
 
-
+app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.json({
@@ -26,13 +26,6 @@ app.use(express.json({
 }));
 
 app.use(express.urlencoded({ extended: true }));
-const corsOptions = {
-  origin: 'https://elv.vercel.app', // Permitir solo esta URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  credentials: true, // Permitir cookies o credenciales
-};
-
-app.use(cors(corsOptions));
 
 app.use(session({
   secret: `${JWT_SECRET_KEY}`,
